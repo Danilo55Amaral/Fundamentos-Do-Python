09fb36b12f2a47269f3a5f45aec1d0ccbdc38a86
Danilo Amaral <danilo123amaral@gmail.com>
2023-07-17 22:16:12 -0300

    :sparkles: feat: Operadores de atribuição

---
+++ operadores-de-atribuicao.py
@@ -0,0 +1,74 @@
+# Operadores de Atribuição
+
+# Esses operadores são utilizados para definir o valor inicial 
+# ou sobrescrever o valor de de uma variável. 
+# Note que podemos ter atribuição simples e também atribuições 
+# envolvendo algum operador aritimetico.
+
+# Atribuição simples
+saldo = 500 
+print(saldo)
+
+# Atribuição com adição 
+saldo = 500 
+saldo += 200
+print(saldo) 
+
+# Atribuição com subtração
+saldo = 200
+saldo -= 100
+print(saldo)
+
+# Atribuição com multiplicação 
+saldo = 900 
+saldo *= 2 
+print(saldo) 
+
+# Atribuição com divisão 
+saldo = 500 
+saldo /= 5 
+print(saldo)
+
+# Divisão inteira
+saldo = 500 
+saldo //= 5 
+print(saldo)
+
+# Atribuição com módulo 
+saldo = 500 
+saldo %= 480 
+print(saldo)
+
+# Atribuição com exponenciação 
+saldo = 80 
+saldo **= 2 
+print(saldo)
+
+# Testando mais alguns exemplos 
+saldo = 500 
+print(saldo) 
+
+# Sobrescrevendo o valor da variável 
+saldo = 200
+print(saldo)
+
+saldo += 10
+print(saldo)
+
+saldo -= 5
+print(saldo) 
+
+saldo /= 2
+print(saldo) 
+
+saldo //= 2
+print(saldo)
+
+saldo *= 10
+print(saldo) 
+
+saldo %= 4 
+print(saldo)
+
+saldo **= 2 
+print(saldo)
